c806de6600afbe4cf896b5ceac5b6b4eed369d6c
sxiao11 <sxiao11@jhu.edu>
2019-07-23 19:44:13 +0800

    first commit

--- qt/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sikai/catkin_ws_project/src/youibot_rrt
-# Build directory: /home/sikai/catkin_ws_project/src/youibot_rrt/qt
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
